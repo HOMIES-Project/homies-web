26e98579065dfbe2e37e924e1a500ee0a37589ef
Esther Lenador <elenadorch@gmail.com>
2022-03-17 14:19:08 +0100

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { ServicesModule } from './services/services.module';
+import { InterceptorsModule } from './interceptors/interceptors.module';
+import { GuardsModule } from './guards/guards.module';
+import { ComponentsModule } from './components/components.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,7 +16,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ComponentsModule,
+    GuardsModule,
+    InterceptorsModule,
+    ServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
