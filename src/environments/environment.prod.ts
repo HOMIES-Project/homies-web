e701051a46d92950ce6cc5354acbcdde6f24c2f8
Ainara Ramos <ainaretework@gmail.com>
2022-03-24 20:32:46 +0100

    Change environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  loginUrl: 'https://homies-1854.herokuapp.com/api/authenticate',
-  registerUrl: 'https://homies-1854.herokuapp.com/api/register',
-  recoveryUrl: 'https://homies-1854.herokuapp.com/api/account/reset-password/init',
-  recoveryUrlKey: 'https://homies-1854.herokuapp.com/api/account/reset-password/finish'
+  loginUrl: 'https://homies-back-app.herokuapp.com/api/authenticate',
+  registerUrl: 'https://homies-back-app.herokuapp.com/api/register',
+  recoveryUrl: 'https://homies-back-app.herokuapp.com/api/account/reset-password/init',
+  recoveryUrlKey: 'https://homies-back-app.herokuapp.com/api/account/reset-password/finish'
 };
