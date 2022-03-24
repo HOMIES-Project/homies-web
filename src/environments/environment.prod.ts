25648c7c75a5552e1a1752dfb2cfa9d8f29789bb
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-03-24 10:43:57 +0100

    added env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  loginUrl: 'https://homies-1854.herokuapp.com/api/authenticate',
+  registerUrl: 'https://homies-1854.herokuapp.com/api/register',
+  recoveryUrl: 'https://homies-1854.herokuapp.com/api/account/reset-password/init',
+  recoveryUrlKey: 'https://homies-1854.herokuapp.com/api/account/reset-password/finish'
 };
