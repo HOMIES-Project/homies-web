d87565d23f488b84276cd0be48df04bd3c41ea2c
Esther Lenador <elenadorch@gmail.com>
2022-03-25 10:20:28 +0100

    added baseURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  loginUrl: 'https://homies-back-app.herokuapp.com/api/authenticate',
-  registerUrl: 'https://homies-back-app.herokuapp.com/api/register',
-  recoveryUrl: 'https://homies-back-app.herokuapp.com/api/account/reset-password/init',
-  recoveryUrlKey: 'https://homies-back-app.herokuapp.com/api/account/reset-password/finish'
+  BASE_URL: 'https://homies-back-app.herokuapp.com/api'
 };
