08ce6f4beb174c93c5d4f48f902e80827981f872
Esther Lenador <elenadorch@gmail.com>
2022-03-31 20:55:48 +0200

    added group creation modal without service

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,6 +7,7 @@ import { TasksComponent } from './tasks/tasks.component';
 import { ExpensesComponent } from './expenses/expenses.component';
 import { GroceriesComponent } from './groceries/groceries.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { GroupCreationComponent } from './group-creation/group-creation.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     ExpensesComponent,
     GroceriesComponent,
     SidebarComponent,
+    GroupCreationComponent,
   ],
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  exports:[GroupCreationComponent]
 })
 export class ComponentsModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,20 +2,47 @@ import { UsersService } from 'src/app/core/services/users.service';
 import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  username!: string;
 
-  username!: string
+  closeResult = '';
 
-  constructor(private usersService: UsersService) {
-    this.usersService.name.subscribe( response => {
-      this.username = response
-    })
+  constructor(private modalService: NgbModal) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
-  ngOnInit(): void {}
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          console.log(reason)
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      console.log('ùlso x')
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      console.log('ùlso y')
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
