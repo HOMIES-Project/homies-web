087260b4e2cb0e65f0bd61e2baa130a56c6bad70
Ainara Ramos <ainaretework@gmail.com>
2022-04-01 16:08:53 +0200

    profile html and svg icons

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,3 +1,5 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UsersService } from 'src/app/core/services/users.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  username!: string
+  name!: string
+  surname!: string
+  email!: string
+  userForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private usersService: UsersService) {
+      
+      this.userForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        name: ['', Validators.required],
+        surname: ['', Validators.required],
+        email: ['', Validators.required, Validators.email],
+      })
+    
+  }
 
   ngOnInit(): void {
   }
