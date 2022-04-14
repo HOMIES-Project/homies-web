a52b098e65e879a05fd7ae129ede83d6053b915e
Juan Buades Ávila <82053845+juanbuades@users.noreply.github.com>
2022-04-14 13:08:10 +0200

    Update sidebar.component.ts

--- src/app/shared-components/sidebar/sidebar.component.ts
+++ src/app/shared-components/sidebar/sidebar.component.ts
@@ -14,9 +14,7 @@ export class SidebarComponent implements OnInit {
   sub: any;
   paramID!: string | null;
 
-
   noGroups!: boolean;
-
 
   constructor(
     private groupsService: GroupsService,
@@ -31,33 +29,29 @@ export class SidebarComponent implements OnInit {
       this.groups = response.groups;
       console.log(response.groups);
     });
+
   }
 
   ngOnInit(): void {
     console.log(this.paramID)
-
 
     //TODO: HANDLE SIDEBAR IF NO GROUPS.
     // if(this.groups?.length == 0) {
     //   this.noGroups = true;
     //   console.log("cero grupos")
     // }
-
+  }
 
   navigateToHomeGroupID(id: string) {
     this.groupsService.updateGroupId(id).subscribe((response) => {
       this.paramID = response;
     });
-
     this.router.navigate(['home', id]);
   }
 
   navigateToTasks() {
     this.groupsService.groupID.subscribe((response) => {
       this.paramID = response;
-
-      console.log(response)
-
     });
     this.router.navigate(['/home', 'tasks', this.paramID], {
       relativeTo: this.route,
@@ -82,6 +76,5 @@ export class SidebarComponent implements OnInit {
       queryParamsHandling: 'preserve',
     });
   }
-
 }
 
