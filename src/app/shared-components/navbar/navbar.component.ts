3c441107697d94b6b8b0bb9c3202c16b98eb8bb7
Esther Lenador <elenadorch@gmail.com>
2022-05-24 19:03:08 +0200

    logout after 15 minutes

--- src/app/shared-components/navbar/navbar.component.ts
+++ src/app/shared-components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { TasksService } from 'src/app/core/services/Lists/tasks.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginModel } from 'src/app/core/models/login.model';
 import { GroupsService } from 'src/app/core/services/groups.service';
 import { UsersService } from 'src/app/core/services/users.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -35,6 +36,8 @@ export class NavbarComponent implements OnInit {
     this.groupsService.groupInfo.subscribe( response =>{
       this.groupName = response?.groupName
     })
+    this.setTimeout();
+    this.userInactive.subscribe(() =>this.logout());
   }
 
   ngOnInit(): void {}
@@ -51,5 +54,16 @@ export class NavbarComponent implements OnInit {
     this.usersService.performLogout();
     this.groupsService.performLogoutFromGroups();
     this.tasksService.performLogoutFromTasks()
+  }
+  userActivity:any;
+  userInactive: Subject<any> = new Subject();
+
+  setTimeout() {
+    this.userActivity = setTimeout(() => this.userInactive.next(undefined), 900000);
+  }
+
+  @HostListener('window:mousemove') refreshUserState() {
+    clearTimeout(this.userActivity);
+    this.setTimeout();
   }
 }
